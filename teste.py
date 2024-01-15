2fd5a38b4add04eb5d0dd163eccd2c92bab7091b
diegofraga <125985752+diegofragadejesus@users.noreply.github.com>
2024-01-15 13:09:13 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,1 +1,18 @@
 print("olá mundo")
+def soma(a ,b):
+  return  a + b
+def sub (a , b):
+  return a - b
+escolha = int(input(f"Escolha uma função da calculadora  \n [1] Pra SOMA \n [2] Para SUBTRAÇÂO \n Digite :"))
+if escolha == 1:
+    num1 = float(input("Esconlha um Numero :"))
+    num2 = float(input("Escolha outro Numero :"))
+    resutado =soma(num1 ,num2)
+    print(f"a soma do valor {num1} + {num2} = {resutado}")
+elif  escolha == 2 :
+    num1 = float(input("Esconlha um Numero :"))
+    num2 = float(input("Escolha outro Numero :"))
+    resutado =sub(num1 ,num2)
+    print(f"a subtração do valor {num1} - {num2} = {resutado :.f2}")
+else: 
+   print ('fim da seção')
