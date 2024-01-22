497d1c0a6be9cea880e9ad122751d46b352b26fc
diegofraga <125985752+diegofragadejesus@users.noreply.github.com>
2024-01-22 08:44:03 -0300

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,9 @@
+#contador = 10
+#while contador >=0:
+   # contador -= 1
+    #print(contador)
+
+not1 =float(input(f'Qual foi sua primeira nota ?'))
+not2 =float(input(f'Qual foi a seu segunda nota ?'))
+media = (not1 + not2) / 2
+print(f'Sua média ficou em :{media :.2f}')
